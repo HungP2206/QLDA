f7f7b77645978a6395b322f213d19d2638e93cf2
HungP2206 <73122366+HungP2206@users.noreply.github.com>
2021-06-05 17:10:31 +0700

    Ví dụ nhập xuất

---
+++ vidu.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ public class vidu{
+    public int a ;
+     public static Scanner sc = new Scanner(System.in);
+    // ham nhap    
+     public static void nhap(int b2) {
+         System.out.println("nhap 1 so ");
+         int b= sc.nextInt();
+     }
+     //ham xuat
+     public static void xuat(int a) {
+         System.out.println("so vua  nhap la: " + a);
+     }
+     public static void main(String args []) 
+     {
+        //Scanner sc = new Scanner(System.in);
+        //public int a ;
+        nhap(a);
+
+        xuat(a);
+     }
+    }
