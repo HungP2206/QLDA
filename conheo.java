5263109acd8e874dba6a63758edf97ffc2d98dd9
HungP2206 <73122366+HungP2206@users.noreply.github.com>
2021-06-05 17:08:17 +0700

    Lần 2
    viết gì đó k nhớ nữa

---
+++ conheo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class conheo {
+    
+    //public class Main {
+        public static int sum(int a, int b) {
+            return a + b;
+        }
+        public static void main(String[] args) {
+            int sum = sum(2,5);
+            System.out.println(sum);
+            
+        }
+    }
